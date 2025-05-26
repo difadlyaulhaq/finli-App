8f50b6485d439f9f57db8405420c0018319616bb
difadlyaulhaq <difadlyaulhaq2@gmail.com>
2025-05-26 22:51:12 +0700

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,72 @@
+import 'package:finli_app/theme/color.dart';
+import 'package:finli_app/widgets/balance_card.dart';
+import 'package:finli_app/widgets/tips_carousel.dart';
+import 'package:finli_app/widgets/transaction_list.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColors.primaryColor,
+        automaticallyImplyLeading: false,
+        title: Row(
+          children: [
+            Text('Helo, ',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+            ),
+            Text('Riyan',
+            style: TextStyle(
+              color: Colors.blue,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+            )
+          ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Image.asset('assets/profile.png'),
+          ),
+        ],
+      ),
+      backgroundColor: AppColors.primaryColor,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: BalanceCard(),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Row(
+                children: [
+                  Text("Latest Transactions",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  ),
+                ],
+              ),
+            ),
+            TransactionList(),
+            TipsCarousel()
+          ],
+        )
+        ),
+    );
+  }
+}
---
+++ lib/theme/color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFFECEDEE);
+  static  Color blue = Colors.blue.shade600;
+  static const Color white = Colors.white;
+}
+
